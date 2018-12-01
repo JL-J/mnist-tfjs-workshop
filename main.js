7dce9cf335283c4f1f7bdb8a870e3ebb583b8347
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-01 10:23:38 +0000

    JS workshop solution

--- main.js
+++ main.js
@@ -36,18 +36,81 @@ function createConvModel() {
  */
 
 function createDenseModel() {
-  // TODO
+  const model = tf.sequential();
+  model.add(tf.layers.flatten({ inputShape: [IMAGE_H, IMAGE_W, 1] }));
+  model.add(tf.layers.dense({ units: 42, activation: "relu" }));
+  model.add(tf.layers.dense({ units: 10, activation: "softmax" }));
+  return model;
 }
 
 /**
  * Train the model with the training data
  */
 async function trainModel() {
-  // TODO
+  MODEL = createDenseModel();
+  MODEL.compile({
+    optimizer: "rmsprop",
+    loss: "categoricalCrossentropy",
+    metrics: ["accuracy"]
+  });
+
+  const trainData = DATA.getTrainData();
+  let trainBatchCount = 0;
+  const totalNumBatches =
+    Math.ceil((trainData.xs.shape[0] * (1 - VALIDATION_SPLIT)) / BATCH_SIZE) *
+    EPOCHS;
+    console.log("🎉 Training Start");
+   await MODEL.fit(trainData.xs, trainData.labels, {
+     batchSize: BATCH_SIZE,
+     validationSplit: VALIDATION_SPLIT, // Leave out the last 15% of the training data for validation, to monitor overfitting during training.
+     epochs: EPOCHS,
+     callbacks: {
+       onBatchEnd: async (batch, logs) => {
+         trainBatchCount++;
+         let percentComplete = (
+           (trainBatchCount / totalNumBatches) *
+           100
+         ).toFixed(1);
+         PROGRESS_UI.setProgress(percentComplete);
+         PROGRESS_UI.setStatus(`ACC ${logs.acc.toFixed(3)}`);
+         console.log(`Training... (${percentComplete}% complete)`);
+         await tf.nextFrame();
+       },
+       onEpochEnd: async (epoch, logs) => {
+         valAcc = logs.val_acc;
+         console.log(`Accuracy: ${valAcc}`);
+         PROGRESS_UI.setStatus(`*ACC ${logs.val_acc.toFixed(3)}`);
+         await tf.nextFrame();
+       }
+     }
+   });
+   console.log("🍾 Training Complete");
+
+
+  // Do a final test of the model with the test data, check it against data it's never seen before!
+  const testData = DATA.getTestData();
+  const testResult = MODEL.evaluate(testData.xs, testData.labels);
+  const testAccPercent = testResult[1].dataSync()[0] * 100;
+  const finalValAccPercent = valAcc * 100;
+  console.log(
+    `Final validation accuracy: ${finalValAccPercent.toFixed(1)}%; ` +
+      `Final test accuracy: ${testAccPercent.toFixed(1)}%`
+  );
 }
 
 function inferModel(data) {
-  // TODO
+  console.log(data);
+  let inputs = tf.tensor4d(data, [1, 28, 28, 1]);
+  // tf.tidy(() => {
+  const output = MODEL.predict(inputs);
+  const distribution = output.dataSync();
+  const axis = 1;
+  const prediction = Array.from(output.argMax(axis).dataSync())[0];
+
+  // });
+  inputs.dispose();
+  output.dispose();
+  return { prediction, distribution };
 }
 
 async function loadAndTrain() {
